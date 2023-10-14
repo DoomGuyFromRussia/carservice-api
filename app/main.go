d81661411090edb6f0784a5d14c946c362002eb8
vlad <voditel.vlad@yandex.ru>
2023-10-14 12:00:15 +0300

    set up

--- app/main.go
+++ app/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	//"mux"
+	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/test", getRoot)
+	http.ListenAndServe(":8010", router)
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "test\n")
 }
