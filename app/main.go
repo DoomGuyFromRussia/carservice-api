2724bcbf70c7f37589e4c164a095b1e1889cb0e3
vlad <voditel.vlad@yandex.ru>
2023-10-16 13:47:47 +0300

    db update

--- app/main.go
+++ app/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	//"mux"
+
 	"fmt"
 	"io"
 	"net/http"
 
+	//"github.com/gorilla/mux"
+	"database/sql"
+
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "db.db")
+	checkErr(err)
+	fmt.Println(db)
 	router := mux.NewRouter()
 	router.HandleFunc("/api/test", getRoot)
 	http.ListenAndServe(":8010", router)
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
