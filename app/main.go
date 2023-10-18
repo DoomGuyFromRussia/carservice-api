97265bdff9357d88ece5c93ba546329047656c09
vlad <voditel.vlad@yandex.ru>
2023-10-18 15:26:43 +0300

    common crud operations for all tables, need validation and errors check

--- app/main.go
+++ app/main.go
@@ -17,12 +17,24 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type IdInt struct {
+	Id int `json:",string"`
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/getCars", getCars)
+	router.HandleFunc("/api/getClients", getClients)
+	router.HandleFunc("/api/getOrders", getOrders)
 	router.HandleFunc("/api/createCar", createCar)
+	router.HandleFunc("/api/createClient", createClient)
+	router.HandleFunc("/api/createOrder", createOrder)
 	router.HandleFunc("/api/updateCar", updateCar)
+	router.HandleFunc("/api/updateClient", updateClient)
+	router.HandleFunc("/api/updateOrder", updateOrder)
 	router.HandleFunc("/api/deleteCar", deleteCar)
+	router.HandleFunc("/api/deleteClient", deleteClient)
+	router.HandleFunc("/api/deleteOrder", deleteOrder)
 	http.ListenAndServe(":8010", router)
 
 }
@@ -36,6 +48,16 @@ func checkErr(err error) {
 func getCars(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("got getCars request")
 	io.WriteString(w, models.GetCars()+"\n")
+}
+
+func getClients(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got getClients request")
+	io.WriteString(w, models.GetClients()+"\n")
+}
+
+func getOrders(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got getOrders request")
+	io.WriteString(w, models.GetOrders()+"\n")
 }
 
 func createCar(w http.ResponseWriter, r *http.Request) {
@@ -57,13 +79,34 @@ func createCar(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func createClient(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got createClient request")
+	decoder := json.NewDecoder(r.Body)
+	var c models.Client
+	err := decoder.Decode(&c)
+	checkErr(err)
+	//validation here
+	io.WriteString(w, models.CreateClient(c)+"\n")
+}
+
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got createOrder request")
+	decoder := json.NewDecoder(r.Body)
+	var o models.Order
+	err := decoder.Decode(&o)
+	checkErr(err)
+	fmt.Println("main desc", o.Description)
+	//validation here
+	io.WriteString(w, models.CreateOrder(o)+"\n")
+}
+
 func updateCar(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("got createCar request")
+	fmt.Println("got updateCar request")
 	decoder := json.NewDecoder(r.Body)
 	var c models.Car
 	err := decoder.Decode(&c)
 	checkErr(err)
-	if c.Model == "" || c.Producer == "" || c.Vin == "" || c.Year == "" {
+	if !carValid(c) {
 		ret := map[string]string{
 			"error": "null values",
 		}
@@ -75,13 +118,51 @@ func updateCar(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func updateClient(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got updateClient request")
+	decoder := json.NewDecoder(r.Body)
+	var c models.Client
+	err := decoder.Decode(&c)
+	checkErr(err)
+	//validation here
+	io.WriteString(w, models.UpdateClient(c)+"\n")
+}
+
+func updateOrder(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got updateOrder request")
+	decoder := json.NewDecoder(r.Body)
+	var o models.Order
+	err := decoder.Decode(&o)
+	checkErr(err)
+	//validation here
+	io.WriteString(w, models.UpdateOrder(o)+"\n")
+}
+
 func deleteCar(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("got deleteCar request")
 	decoder := json.NewDecoder(r.Body)
-	var id int
-	err := decoder.Decode(&id)
+	var j IdInt
+	err := decoder.Decode(&j)
 	checkErr(err)
-	io.WriteString(w, models.DeleteCar(id)+"\n")
+	io.WriteString(w, models.DeleteCar(j.Id)+"\n")
+}
+
+func deleteClient(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got deleteClient request")
+	decoder := json.NewDecoder(r.Body)
+	var j IdInt
+	err := decoder.Decode(&j)
+	checkErr(err)
+	io.WriteString(w, models.DeleteClient(j.Id)+"\n")
+}
+
+func deleteOrder(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("got deleteOrder request")
+	decoder := json.NewDecoder(r.Body)
+	var j IdInt
+	err := decoder.Decode(&j)
+	checkErr(err)
+	io.WriteString(w, models.DeleteOrder(j.Id)+"\n")
 }
 
 // i need validator
@@ -93,4 +174,13 @@ func carValid(c models.Car) bool {
 		return true
 	}
 
+}
+
+func sendErrorJson(w http.ResponseWriter, r *http.Request) {
+	ret := map[string]string{
+		"status": "error",
+	}
+	r_, err := json.Marshal(ret)
+	checkErr(err)
+	io.WriteString(w, string(r_))
 }
--- app/models/models.go
+++ app/models/models.go
@@ -20,20 +20,20 @@ type Car struct {
 }
 
 type Client struct {
-	id      int
-	name    string
-	surname string
-	address string
-	phone   string
+	Id      int `json:"id,string,omitempty"`
+	Name    string
+	Surname string
+	Address string
+	Phone   string
 }
 
 type Order struct {
-	id          int
-	carId       int
-	clientId    int
-	date        string
-	description string
-	status      string
+	Id          int `json:"id,string,omitempty"`
+	CarId       int
+	ClientId    int
+	Date        string
+	Description string
+	Status      string
 }
 
 var Db *sql.DB
@@ -63,12 +63,69 @@ func GetCars() string {
 	return enc
 }
 
+func GetClients() string {
+	InitDb()
+	rows, err := Db.Query("select * from clients")
+	checkErr(err)
+	var clients []Client
+	for rows.Next() {
+		var c Client
+		err := rows.Scan(&c.Id, &c.Name, &c.Surname, &c.Address, &c.Phone)
+		checkErr(err)
+		clients = append(clients, c)
+	}
+	defer Db.Close()
+	b, err := json.Marshal(clients)
+	enc := string(b)
+	checkErr(err)
+	return enc
+}
+
+func GetOrders() string {
+	InitDb()
+	rows, err := Db.Query("select * from orders")
+	checkErr(err)
+	var orders []Order
+	for rows.Next() {
+		var o Order
+		err := rows.Scan(&o.Id, &o.CarId, &o.ClientId, &o.Date, &o.Description, &o.Status)
+		checkErr(err)
+		orders = append(orders, o)
+	}
+	defer Db.Close()
+	b, err := json.Marshal(orders)
+	enc := string(b)
+	checkErr(err)
+	return enc
+}
+
 func CreateCar(c Car) string {
 	InitDb()
 	res, err := Db.Exec("insert into cars (producer, model, year, vin) values ($1, $2, $3, $4)", c.Producer, c.Model, c.Year, c.Vin)
 	checkErr(err)
 	fmt.Println(res)
 	r, err := json.Marshal(c)
+	checkErr(err)
+	return string(r)
+}
+
+func CreateClient(c Client) string {
+	InitDb()
+	res, err := Db.Exec("insert into clients (name, surname, address, phone) values ($1, $2, $3, $4)", c.Name, c.Surname, c.Address, c.Phone)
+	checkErr(err)
+	fmt.Println(res)
+	r, err := json.Marshal(c)
+	checkErr(err)
+	return string(r)
+}
+
+func CreateOrder(o Order) string {
+	InitDb()
+	fmt.Println("Desc", o.Description)
+	res, err := Db.Exec("insert into orders (carId, clientId, date, description, status) values ($1, $2, $3, $4, $5)", o.CarId, o.ClientId, o.Date, o.Description, o.Status)
+	checkErr(err)
+	fmt.Println(res)
+	r, err := json.Marshal(o)
 	checkErr(err)
 	return string(r)
 }
@@ -85,9 +142,62 @@ func UpdateCar(c Car) string {
 	checkErr(err)
 	return string(r)
 }
+
+func UpdateClient(c Client) string {
+	InitDb()
+	res, err := Db.Exec("update clients set name = $1, surname = $2, address = $3, phone = $4 where id = $5", c.Name, c.Surname, c.Address, c.Phone, c.Id)
+	checkErr(err)
+	fmt.Println(res.RowsAffected())
+	ret := map[string]string{
+		"result": "ok",
+	}
+	r, err := json.Marshal(ret)
+	checkErr(err)
+	return string(r)
+}
+
+func UpdateOrder(o Order) string {
+	InitDb()
+	res, err := Db.Exec("update orders set carId = $1, clientId = $2, date = $3, description = $4, status = $5 where id = $6", o.CarId, o.ClientId, o.Date, o.Description, o.Status, o.Id)
+	checkErr(err)
+	fmt.Println(res.RowsAffected())
+	ret := map[string]string{
+		"result": "ok",
+	}
+	r, err := json.Marshal(ret)
+	checkErr(err)
+	return string(r)
+}
+
 func DeleteCar(id int) string {
 	InitDb()
 	res, err := Db.Exec("delete from cars where id = $1", id)
+	checkErr(err)
+	fmt.Println(res.RowsAffected())
+	ret := map[string]string{
+		"result": "ok",
+	}
+	r, err := json.Marshal(ret)
+	checkErr(err)
+	return string(r)
+}
+
+func DeleteClient(id int) string {
+	InitDb()
+	res, err := Db.Exec("delete from clients where id = $1", id)
+	checkErr(err)
+	fmt.Println(res.RowsAffected())
+	ret := map[string]string{
+		"result": "ok",
+	}
+	r, err := json.Marshal(ret)
+	checkErr(err)
+	return string(r)
+}
+
+func DeleteOrder(id int) string {
+	InitDb()
+	res, err := Db.Exec("delete from orders where id = $1", id)
 	checkErr(err)
 	fmt.Println(res.RowsAffected())
 	ret := map[string]string{
